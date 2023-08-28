44a2f66a0b77ac6b4673d5da01c35b97423440ba
mayankjain-plivo <mayank.jain@plivo.com>
2023-08-28 13:07:50 +0530

    CNTO-7095 updated variable names

--- src/components/TImeLeftBar/index.stories.js
+++ src/components/TImeLeftBar/index.stories.js
@@ -12,13 +12,13 @@ const Template = (args) => <TimeLeftBar {...args} />
 export const Default = Template.bind({})
 Default.args = {
   duration: 20000, // total duration of bar
-  colorone: 'success-color', // Color of the bar is it's percentage is more than progressPercentage
-  colortwo: 'danger-color', // Color of the  bar is it's percentage is less than progressPercentage
-  coloronethreshold: 25, // Percent at which the  bar changes it's color
+  colorOne: 'success-color', // Color of the bar is it's percentage is more than progressPercentage
+  colorTwo: 'danger-color', // Color of the  bar is it's percentage is less than progressPercentage
+  colorThreshold: 25, // Percent at which the  bar changes it's color
 }
 
 export const WithoutThreshold = Template.bind({})
 WithoutThreshold.args = {
   duration: 20000,
-  colorone: 'primary-color',
+  colorOne: 'primary-color',
 }
