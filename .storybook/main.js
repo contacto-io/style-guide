cfc8ec6e170de40f7ce444bbaed63eb4febc0d43
Samuel Lawrentz <samuel.lawerence@plivo.com>
2024-04-04 20:55:30 +0530

    WIP - Made it work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,42 @@
+
+import { mergeConfig } from 'vite'
+import path from 'path'
+import svgr from 'vite-plugin-svgr'
+
 module.exports = {
-  "stories": ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-mdx-gfm"
-  ],
-
-  framework: {
-    name: "@storybook/react-vite",
-    options: {}
-  },
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-mdx-gfm'],
 
   staticDirs: ['../static'],
 
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  framework: {
+    name: '@storybook/react-vite',
+    options: {},
+  },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
+  async viteFinal(config) {
+    // Merge custom configuration into the default config
+    return mergeConfig(config, {
+      plugins: [
+        svgr({
+          svgrOptions: {
+            exportType: 'default',
+          },
+        }),
+      ],
+      resolve: {
+        alias: [
+          { find: '~antd', replacement: path.resolve(__dirname, '../node_modules/antd') },
+          { find: '~simplebar', replacement: path.resolve(__dirname, '../node_modules/simplebar') },
+        ],
+      },
+    })
+  },
 }
+
